f862c18adc1041b43054cc39cf453e6c527ee50a
Artem <a.v.shiliaev@gmail.com>
2020-10-18 10:15:19 +0200

    prototype

---
+++ Creational/Prototype/IdInfo.cs
@@ -0,0 +1,12 @@
+namespace Creational.Prototype
+{
+    public class IdInfo
+    {
+        public int IdNumber;
+
+        public IdInfo(int idNumber)
+        {
+            IdNumber = idNumber;
+        }
+    }
+}
